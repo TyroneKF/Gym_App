daa2c97ee23a428995777e197950bf0ec6b6c876
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-01-02 04:36:59 +0000

    Updates: Creating edit IngredientsTypes & Further update GUI
    Need To do:
    refresh button on addIngredients suppliers
    make ingredientsForm and addShops not editable when an ingredient isnt selected.
    
    Make all variables in private class private and have accessor and mutator methods
    
    Pass IngredientsTypesToNames data between classes - saves processing
    Opening and closing editscreen without changing anything resets saves, table
    Save doesnt display message on empty tahble - saves no message
    
    ingredients_info - Ingredient_Type store ID
    ingredientInShops  - Store_Name make store ID
    
    IngredientType DB taBLE
    Add ingredient Type
    Add a way to add shops
    select ingredientsType in editIngredientInfo
    
    Only Update IngredientType where the previoustype and new type change.
    
    refactor trigger columns
    Repeated message on ingredient form with ingredient with no supplier info!
    
    Change tableForm how data is stored to make when the ingredient info is changed easier

---
+++ src/App_Code/Objects/Screens/IngredientsTypesScreen.java
@@ -0,0 +1,13 @@
+package App_Code.Objects.Screens;
+
+import App_Code.Objects.Gui_Objects.CollapsibleJPanel;
+import App_Code.Objects.Gui_Objects.IconButton;
+import App_Code.Objects.Gui_Objects.IconPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/*
+
+
+ */
