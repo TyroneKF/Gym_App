9c963d66f3104380d3b51c9b805e8b6fd2cb9c04
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-01-04 22:57:18 +0000

    Updates:                                   Reformat Updates
    ##########################################################################
    Need To do:
    Supplier Delete action
    Supplier Page needs to reload forms shop info
    
    refresh button on addIngredients suppliers
    make ingredientsForm and addShops not editable when an ingredient isnt selected.
    
    Make all variables in private class private and have accessor and mutator methods
    
    Pass IngredientsTypesToNames data between classes - saves processing
    Opening and closing editscreen without changing anything resets saves, table
    Save doesnt display message on empty tahble - saves no message
    
    ingredients_info - Ingredient_Type store ID
    ingredientInShops  - Store_Name make store ID
    
    IngredientType DB taBLE
    Add ingredient Type
    Add a way to add shops
    select ingredientsType in editIngredientInfo
    
    Only Update IngredientType where the previoustype and new type change.
    
    refactor trigger columns
    Repeated message on ingredient form with ingredient with no supplier info!
    
    Change tableForm how data is stored to make when the ingredient info is changed easier

--- src/App_Code/Objects/Screens/Others/MealPlanScreen.java
+++ src/App_Code/Objects/Screens/Others/MealPlanScreen.java
@@ -184,7 +184,7 @@ public class MealPlanScreen extends JPanel
         //HELLO REMOVE
         System.out.printf("\n\nChosen Plan: %s", planID);
         System.out.println("\n\n###############################################################################");
-/*
+
         if (planID != null)
         {
             //####################################################
@@ -448,7 +448,7 @@ public class MealPlanScreen extends JPanel
             }
         });
 
- */
+
 
         open_AddIngredients_Screen();
     }
