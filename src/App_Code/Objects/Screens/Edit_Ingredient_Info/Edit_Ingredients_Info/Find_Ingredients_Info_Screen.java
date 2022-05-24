12ce6026837168ca81541c0c20c90518b868f0b8
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-05-24 19:39:25 +0100

    Updates Made in this commit: ). Refactoring
    Trying to do in this update:
    .) Trying to get sizing right on search bar
    
    Next To Do:
    .) Add Interface for requesting ingredient nutrientional info
    
    Errors:
    .) scrollBar_Down to bottom doesn't scroll all the way to the bottom when adding a meal.
    
    Need to update:
    .) ScrollBar_Down BTN incorporated & into add new meal functionality.
    
    .) Delete folder in path: "src/Resources/Database Docs/New Setup3" if not needed.
    
    .) Edit_Ingredients_Types_Screen() - doesnt remove space in ingredientType name provided when adding it to the db - check Edit_Ingredient_Stores_Screen() too.
    
    .) Cant Update ingredient price if its in a meal.
    
    .) Any edits to ingredients needs to update meals

--- src/App_Code/Objects/Screens/Edit_Ingredient_Info/Edit_Ingredients_Info/Find_Ingredients_Info_Screen.java
+++ src/App_Code/Objects/Screens/Edit_Ingredient_Info/Edit_Ingredients_Info/Find_Ingredients_Info_Screen.java
@@ -1,37 +1,230 @@
 package App_Code.Objects.Screens.Edit_Ingredient_Info.Edit_Ingredients_Info;
 
-import App_Code.Objects.Gui_Objects.CollapsibleJPanel;
 import App_Code.Objects.API.Nutritionix.NutritionIx_API;
+import App_Code.Objects.Gui_Objects.IconButton;
+import App_Code.Objects.Gui_Objects.JTextFieldLimit;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.LinkedHashMap;
 
-public class Find_Ingredients_Info_Screen extends CollapsibleJPanel
+public class Find_Ingredients_Info_Screen extends JPanel
 {
-    NutritionIx_API nutritionIx_api;
-    Edit_Ingredients_Screen.CreateForm.IngredientsForm ingredientsForm;
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private JPanel screenSectioned, centerJPanel;
+
+    private NutritionIx_API nutritionIx_api;
+    private Edit_Ingredients_Screen.CreateForm.IngredientsForm ingredientsForm;
+
+    private int
+            frameWidth = 710, frameHeight = 850,
+
+    titleJPanelHeight = 45, titleFontSize = 16,
+
+    searchBarTxtInputSize = 13,
+
+    searchBarHeight = titleJPanelHeight,
+            searchBarButtonWidth = 70,
+
+    searchBarWidth = frameWidth - (searchBarButtonWidth + 20),
+
+    searchBarIconWidth = searchBarButtonWidth - 10,
+            searchBarButtonHeight = searchBarHeight,
+            searchBarIconHeight = searchBarButtonHeight;
 
 
-    public Find_Ingredients_Info_Screen(Container parentContainer, String btnText, int btnWidth, int btnHeight,  Edit_Ingredients_Screen.CreateForm.IngredientsForm ingredientsForm)
+    Container contentPane;
+
+
+    public static void main(String[] args)
     {
-        super(parentContainer, btnText, btnWidth, btnHeight);
+        new Find_Ingredients_Info_Screen(null, null);
+    }
+
+    public Find_Ingredients_Info_Screen(Container parentContainer, Edit_Ingredients_Screen.CreateForm.IngredientsForm ingredientsForm)
+    {
         this.ingredientsForm = ingredientsForm;
         nutritionIx_api = new NutritionIx_API();
 
-        expandJPanel();
+        createGUI();
+    }
+
+    public void createGUI()
+    {
+        //#############################################################################################################
+        //   1. Create the frame.
+        //#############################################################################################################
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(frameWidth, frameHeight);
+        frame.setLocation(00, 0);
+
+        //########################################################
+        //  Contentpane
+        //########################################################
+
+        contentPane = frame.getContentPane();
+        contentPane.setLayout(new GridLayout(1, 1));
+        contentPane.setVisible(true);
+
+        //########################################################
+        // Creating TabbedPane
+        //########################################################
+        JTabbedPane tp = new JTabbedPane();
+        contentPane.add(tp);
+
+        //##############################################################################################################
+        //  Creating JPanel Sections
+        //##############################################################################################################
+
+        screenSectioned = new JPanel(new BorderLayout());
+        tp.add("Add Ingredients", screenSectioned);
+
+
+        //##############################################################################################################
+        //  Creating JPanel Sections
+        //##############################################################################################################
+        //   screenSectioned = new JPanel(new BorderLayout());
+
+        //#####################################################################
+        //  North JPanel
+        //#####################################################################
+        JPanel mainNorthPanel = new JPanel(new BorderLayout());
+        mainNorthPanel.setBackground(Color.RED);
+        screenSectioned.add(mainNorthPanel, BorderLayout.NORTH);
+
+        //####################################
+        // Label
+        //####################################
+        JLabel titleLabel = new JLabel("Search For Ingredient Info");
+        titleLabel.setFont(new Font("Verdana", Font.PLAIN, titleFontSize));
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JPanel titlePanel = new JPanel();
+        titlePanel.setPreferredSize(new Dimension(frameWidth, titleJPanelHeight));
+        titlePanel.setBackground(Color.green);
+        titlePanel.add(titleLabel);
+
+        // Add title JPanel to North Panel Area
+        mainNorthPanel.add(titlePanel, BorderLayout.CENTER);
+
+        //#########################################################################################################
+        //  Centre JPanel
+        //##########################################################################################################
+        JPanel mainCenterPanel = new JPanel(new BorderLayout());
+        screenSectioned.add(mainCenterPanel, BorderLayout.CENTER);
+
+        //#################################################################
+        // Search Bar JPanel
+        //#################################################################
+        // Adding the main section of search Bar JPanel GUI to display
+        JPanel searchBarJPanel = new JPanel(new BorderLayout());
+        mainCenterPanel.add(searchBarJPanel, BorderLayout.NORTH);
+  /*
+        //###########################################
+        // Search Bar West Panel
+        //##########################################
+        // Creating JPanel for text input area for search bar
+        JPanel searchBarWestJPanel = new JPanel(new GridLayout(1, 1));
+        searchBarWestJPanel.setPreferredSize(new Dimension(searchBarWidth , searchBarHeight));
+        searchBarWestJPanel.setBackground(Color.BLUE);
+
+        // adding JPanel for text input for search bar to GUI
+        searchBarJPanel.add(searchBarWestJPanel, BorderLayout.WEST);
+
+        //####################
+        // Text Field
+        //####################
+
+        // Creating Text input object for search bar
+        JTextField textField = new JTextField("");
+
+        textField.setFont(new Font("Verdana", Font.PLAIN, searchBarTxtInputSize)); // Changing font size
+        textField.setDocument(new JTextFieldLimit(255));  // Setting charecter limit
+        textField.setHorizontalAlignment(JTextField.CENTER); // Centering JTextField Text
+
+        // Adding to GUI
+        searchBarWestJPanel.add(textField); // adding textField to JPanel*/
+
+        //#########################################
+        // Search Bar East Panel
+        //#########################################
+        // Creating JPanel for  search bar icon
+        JPanel searchBarEastJPanel = new JPanel(new GridBagLayout());
+
+        // adding JPanel for search bar Icon to GUI
+        searchBarJPanel.add(searchBarEastJPanel, BorderLayout.EAST);
+
+        // Create Search Icon BTN
+        IconButton searchIcon = new IconButton("src/images/search/search2.png", "", 35, 35, 45, 45,
+                "centre", "right");
+
+        addToContainer(searchBarEastJPanel, searchIcon, 0, 2, 1, 1, 0.25, 0.25, "both", 0, 0);
+
+
+        //##############################################################################################################
+        //  Resizing GUI
+        //##############################################################################################################
+
+        resizeGUI(); // Resize GUI
+    }
+
+    public void resizeGUI()
+    {
+        contentPane.revalidate();
+        screenSectioned.revalidate();
     }
 
     public void findFoodInfo(String food)
     {
         LinkedHashMap<String, Object> foodInfo = nutritionIx_api.getFoodNutritionalInfo(food);
 
-        if(foodInfo == null)
+        if (foodInfo == null)
         {
-            JOptionPane.showMessageDialog( null,String.format("\n\nError \n\nUnable to get nutritional info for the requested food '%s'!", food));
+            JOptionPane.showMessageDialog(null, String.format("\n\nError \n\nUnable to get nutritional info for the requested food '%s'!", food));
             return;
         }
-        JOptionPane.showMessageDialog( null,String.format("\n\nSuccessfully got the nutritional info for the food '%s'!", food));
+
+        //##################################
+        // Change T
+        //##################################
+
+        JOptionPane.showMessageDialog(null, String.format("\n\nSuccessfully got the nutritional info for the food '%s'!", food));
+
 
     }
+
+    private void addToContainer(Container container, Component addToContainer, int gridx, int gridy, int gridwidth,
+                                int gridheight, double weightx, double weighty, String fill, int ipady, int ipadx)
+    {
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.gridwidth = gridwidth;
+        gbc.gridheight = gridheight;
+        gbc.weightx = weightx;
+        gbc.weighty = weighty;
+
+        gbc.ipady = ipady;
+        gbc.ipadx = ipadx;
+
+        switch (fill.toLowerCase())
+        {
+            case "horizontal":
+                gbc.fill = GridBagConstraints.HORIZONTAL;
+                break;
+            case "vertical":
+                gbc.fill = GridBagConstraints.VERTICAL;
+                break;
+
+            case "both":
+                gbc.fill = GridBagConstraints.BOTH;
+                break;
+        }
+
+        container.add(addToContainer, gbc);
+    }
+
+
 }
