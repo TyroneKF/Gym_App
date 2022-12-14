4ce0f93d27a9c6767969bb0f983159a51b06a6f2
Tyrone Friday <tyrone.f2000@outlook.com>
2022-12-14 19:06:21 +0000

    Updates Made in this commit: .) Created DividedMealsTable table
    Previous Update:
    .)  Bug - deleting table
    .) Fixed -  update by quantity and name, store IngredientsTa.ble.java
    .)  Fixed - Save Method IngredientsTable.java
    .) Fixed - Refresh Method IngredientsTable.java
    .) Fixed - DeleteRowAction Method IngredientsTable.java
    
    Trying to do in this update:
    .) deleteTableAction() doesn't work as intended, it needs to hide just that specific  meal.
    
    Errors:
    .) deleteTableAction() doesn't work as intended, it needs to hide just that specific  meal.
    
    Next To Do:
    .) Efficient way of saving plan whereby only meals that are changed processed.
    
    .) StoresList
    
    .) Closing Parent_Ingredients_Info ....
    brings up table successfully deleted
    
    .) if ingredientType / ingredientName changes the form should update (meal Plan)
    
    Need to update:
    .) ScrollBar_Down BTN incorporated & into add new meal functionality.
    
    .) Delete folder in path: "src/Resources/Database Docs/New Setup3" if not needed.
    
    .) Edit_Ingredients_Types_Screen() - doesnt remove space in ingredientType name provided when adding it to the db - check Edit_Ingredient_Stores_Screen() too.
    
    .) Cant Update ingredient price if its in a meal.
    
    .) Any edits to ingredients needs to update meals

---
+++ src/App_Code/Objects/Database_Objects/JTable_JDBC/Children/ViewDataTables/Children/DividedMealsTable.java
@@ -0,0 +1,37 @@
+package App_Code.Objects.Database_Objects.JTable_JDBC.Children.ViewDataTables.Children;
+
+import App_Code.Objects.Database_Objects.JDBC.MyJDBC;
+import App_Code.Objects.Gui_Objects.CollapsibleJPanel;
+
+import java.util.ArrayList;
+
+public class DividedMealsTable extends TotalMealTable
+{
+    Integer divMealSectionsID;
+
+    private DividedMealsTable(MyJDBC db, CollapsibleJPanel collapsibleObj, String databaseName, Object[][] data, String[] columnNames, int planID, Integer MealInPlanID, String mealName, String tableName, ArrayList<String> unEditableColumns, ArrayList<String> colAvoidCentering, ArrayList<String> columnsToHide)
+    {
+        super(db, collapsibleObj, databaseName, data, columnNames, planID, MealInPlanID, mealName, tableName, unEditableColumns, colAvoidCentering, columnsToHide);
+    }
+
+    public DividedMealsTable(MyJDBC db, CollapsibleJPanel collapsibleObj, String databaseName, Object[][] data, String[] columnNames, int planID, Integer MealInPlanID, Integer divMealSectionsID, String mealName, String tableName, ArrayList<String> unEditableColumns, ArrayList<String> colAvoidCentering, ArrayList<String> columnsToHide)
+    {
+        super(db, collapsibleObj, databaseName, data, columnNames, planID, MealInPlanID, mealName, tableName, unEditableColumns, colAvoidCentering, columnsToHide);
+        this.divMealSectionsID = divMealSectionsID;
+    }
+
+    @Override
+    public void updateTotalMealTable()
+    {
+        String query = String.format("SELECT *  FROM divided_meal_sections_calculations WHERE DivMealSectionsID = %s AND PlanID = %s;", divMealSectionsID, super.temp_PlanID);
+        super.updateTable("DividedMealsTable", query, 1);
+    }
+
+    @Override
+    public boolean updateTotalMealTableModelData()
+    {
+        String query = String.format("SELECT * FROM divided_meal_sections_calculations WHERE DivMealSectionsID = %s AND PlanID = %s;", divMealSectionsID, super.temp_PlanID);
+        return super.updateTableModelData("divided_meal_sections_calculations", query);
+    }
+
+}
