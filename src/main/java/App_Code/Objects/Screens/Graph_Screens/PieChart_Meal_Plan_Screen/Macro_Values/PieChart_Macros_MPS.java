f704d90783e7affd23cc8c93afa1901cd4d792b5
Tyrone Friday <tyrone.f2000@outlook.com>
2025-10-13 22:42:00 +0100

    Updates Made in this commit: .) Refactoring Screens Object
    Next To Do:
    .)
    
    Previous Updates:
    .)
    
    Errors
    .)

--- src/main/java/App_Code/Objects/Screens/Graph_Screens/PieChart_Meal_Plan_Screen/Macro_Values/PieChart_Macros_MPS.java
+++ src/main/java/App_Code/Objects/Screens/Graph_Screens/PieChart_Meal_Plan_Screen/Macro_Values/PieChart_Macros_MPS.java
@@ -1,9 +1,9 @@
 package App_Code.Objects.Screens.Graph_Screens.PieChart_Meal_Plan_Screen.Macro_Values;
 
 import App_Code.Objects.Database_Objects.JDBC.MyJDBC;
-import App_Code.Objects.Gui_Objects.Screen;
+import App_Code.Objects.Gui_Objects.Screens.Screen_JFrame;
 
-public class PieChart_Macros_MPS extends Screen
+public class PieChart_Macros_MPS extends Screen_JFrame
 {
     //##################################################################################################################
     // Variables
