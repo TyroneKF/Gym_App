d9b0e1182acf8ef566b5d3ca13c73fa23cf555f5
Tyrone Friday <tyrone.f2000@outlook.com>
2025-10-20 04:53:06 +0100

    Updates Made in this commit: .) Refactored LineChart Code into one screen
    Next To Do:
    .) Empty Pie chart has weird values
    
    Previous Updates:
    .)
    
    Errors
    .)

--- src/main/java/App_Code/Objects/Screens/Graph_Screens/LineChart_Meal_Plan_Screen/LineChart_Macros_MPS.java
+++ src/main/java/App_Code/Objects/Screens/Graph_Screens/LineChart_Meal_Plan_Screen/LineChart_Macros_MPS.java
@@ -13,6 +13,7 @@ import org.jfree.data.time.TimeSeriesCollection;
 import java.awt.*;
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class LineChart_Macros_MPS extends Screen_JPanel
 {
@@ -33,6 +34,15 @@ class LineChart_Macros_MPS extends Screen_JPanel
     // Collections
     //##############################################
     private ArrayList<String> macros_To_Check;
+    
+    /**
+     * HashMap<String, HashMap<MealManager, BigDecimal>> mealManagers_TotalMeal_MacroValues = new HashMap<>();
+     * Stores all the mealManagers TotalMealValues in collections by the macroName
+     * <p>
+     * <Key: MacroName | Value: HashMap <Key: MealManager, Value:  Quantity>>
+     * Etc;  <Key: Salt | Value: HashMap<MealManager, Quantity: 300g >>
+     */
+    private LinkedHashMap<String, HashMap<MealManager, BigDecimal>> mealManagers_TotalMeal_MacroValues;
     
     //##############################################
     // Datasets Objects
@@ -55,11 +65,17 @@ class LineChart_Macros_MPS extends Screen_JPanel
         // ##########################################
         // Variables
         // ##########################################
+        // Objects
         this.db = db;
         this.meal_plan_screen = meal_plan_screen;
         this.mealManagerRegistry = meal_plan_screen.get_MealManagerRegistry();
+       
+        // String
         this.planName = meal_plan_screen.getPlanName();
+       
+        // Collections
         this.macros_To_Check = macros_To_Check;
+        this.mealManagers_TotalMeal_MacroValues = mealManagerRegistry.get_MealManagers_MacroValues();
         
         //############################################
         // Creating Macros / Dataset
@@ -73,12 +89,67 @@ class LineChart_Macros_MPS extends Screen_JPanel
         addToContainer(getScrollPaneJPanel(), line_chart, 0, getAndIncreaseContainerYPos(), 1, 1, 0.25, 0.25, "both", 0, 0, null);
     }
     
-    // ##################################################
+    // #################################################################################################################
     // Build Methods
-    // ##################################################
+    // #################################################################################################################
     private void createDataSet()
     {
-        dataset = mealManagerRegistry.create_Plan_MacroValues_LineChart(macros_To_Check);
+        dataset = new TimeSeriesCollection();
+        
+        for (String macroName : macros_To_Check)
+        {
+            
+            // Create a series for each macroName
+            TimeSeries macroTimeSeries = new TimeSeries(convert_MacroName_To_GUI_Version(macroName));
+            dataset.addSeries(macroTimeSeries);
+            
+            // Add all the values from this macroName into the series
+            /**
+             * HashMap<String, Map<Integer, Pair<LocalTime, BigDecimal>>> mealManagersMacroValues = new HashMap<>();
+             * Stores all the mealManagers TotalMealValues in collections by the macroName
+             * <Key: MacroName | Value: Map <Key: MealManagerID, Value: < MealTime, Quantity>>
+             * Etc;  <Key: Salt | Value: <MealManagerID: 1, <MealTime: 14:00 , Quantity: 300g >>
+             */
+            
+            HashMap<MealManager, BigDecimal> macroValues = mealManagers_TotalMeal_MacroValues.get(macroName);
+            Iterator<Map.Entry<MealManager, BigDecimal>> it = macroValues.entrySet().iterator();
+            
+            while (it.hasNext()) // Iterate through the recorded MealManager Values for this macro
+            {
+                Map.Entry<MealManager, BigDecimal> mealManagers_Info = it.next();
+                
+                // Add time and Value for MealManager
+                MealManager mealManager = mealManagers_Info.getKey();
+                BigDecimal macroValue = mealManagers_Info.getValue();
+                
+                macroTimeSeries.add(mealManager.getCurrentMealTime(), macroValue);
+            }
+        }
+    }
+    
+    private String convert_MacroName_To_GUI_Version(String macroName)
+    {
+        // Capitalize and split word
+        macroName = Arrays.stream(macroName.split("[ _]+"))
+                .map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
+                .collect(Collectors.joining(" "));
+        
+        
+        // Reformat macroName for GUI purposes  "\u00A0" is like space because \t doesn't work in this label format
+        return String.format("\u00A0\u00A0%s\u00A0\u00A0", macroName);
+    }
+
+    // #################################################################################################################
+    // Methods
+    // #################################################################################################################
+    
+    //##################################################
+    //  Update  Methods
+    //##################################################
+    public void update_LineChart_Title()
+    {
+        planName = meal_plan_screen.getPlanName();
+        line_chart.setTitle(planName);
     }
     
     /**
@@ -101,7 +172,6 @@ class LineChart_Macros_MPS extends Screen_JPanel
         // ####################################################
         // Get MealManager MacroInfo & Replace
         // ####################################################
-        LinkedHashMap<String, HashMap<MealManager, BigDecimal>> updated_Data = mealManagerRegistry.get_MealManagers_MacroValues();
         Iterator<String> it = macros_To_Check.iterator();
         
         while (it.hasNext())
@@ -117,12 +187,12 @@ class LineChart_Macros_MPS extends Screen_JPanel
             /**
              *  <Key: MacroName | Value: HashMap<Key: MealManagerID, Value: < MealManager, Quantity>>
              */
-    
+            
             // Get Macro Info Specific to this mealManager correlating to macroName
-            BigDecimal newMacroValue = updated_Data.get(macroName).get(mealManager);
-      
+            BigDecimal newMacroValue = mealManagers_TotalMeal_MacroValues.get(macroName).get(mealManager);
+            
             // Convert Table Column to Key in TimeSeries Collection
-            String macroNameGUI = convertMacroNameToGuiVersion(macroName);
+            String macroNameGUI = convert_MacroName_To_GUI_Version(macroName);
             
             // #######################################
             // Same Time: Update MacroValue
@@ -142,29 +212,6 @@ class LineChart_Macros_MPS extends Screen_JPanel
             macroSeries.delete(previousTime); // IF the time has changed delete the old time from series value
             macroSeries.add(currentTime, newMacroValue); // Add new value to Series with new Time
         }
-    }
-    
-    // #################################################################################################################
-    // Methods
-    // #################################################################################################################
-    
-    
-    //##################################################
-    // Conversion Methods
-    //##################################################
-    private String convertMacroNameToGuiVersion(String macroName)
-    {
-        // Reformat macroName for GUI purposes  "\u00A0" is like space because \t doesn't work in this label format
-        return String.format("\u00A0\u00A0%s\u00A0\u00A0", formatStrings(macroName, true));
-    }
-    
-    //##################################################
-    //  Update  Methods
-    //##################################################
-    public void update_LineChart_Title()
-    {
-        planName = meal_plan_screen.getPlanName();
-        line_chart.setTitle(planName);
     }
     
     //##################################################
