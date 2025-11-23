c97499bc88450db0c5192b7a662390ad7ae26ae0
Tyrone Friday <tyrone.f2000@outlook.com>
2025-11-23 10:48:04 +0000

    Updates Made in this commit: .) DB Update
    Next To Do:
    
    .) Implement update other screens method on add / edit screens.
    
    .) Refactor MPS add meal object with new methods
    
    Previous Updates:
    .)
    
    Errors
    .)

--- target/classes/data/database_scripts/6.) measurements.sql
+++ target/classes/data/database_scripts/6.) measurements.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO measurements ( meassurement_id, unit_name, unit_symbol, measured_material_type) VALUES
+INSERT IGNORE INTO measurements ( measurement_id, unit_name, unit_symbol, measured_material_type) VALUES
 (1, 'Litres', 'L', 'Liquids'),
 (2, 'Grams', 'g', 'Solids');
