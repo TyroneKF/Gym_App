1af542f69058d755fd2194f3f053963dc7a1951c
Tyrone Friday <tyrone.f2000@outlook.com>
2025-12-24 23:32:28 +0000

    Updates Made in this commit: .) Script Update V14
    Next To Do:
    .) Use migration runner & log errors
    .) Edit Screen delete / refresh btn
    
    Previous Updates:
    .)
    
    Errors
    .)

--- src/main/resources/data/db/migrations/V14__seed_macros_versions.sql
+++ src/main/resources/data/db/migrations/V14__seed_macros_versions.sql
@@ -5,7 +5,11 @@ INSERT INTO macros_per_pound_and_limits (macros_ID) VALUES
 (null);
 
 SET @macros_id := LAST_INSERT_ID();
-	
+
+-- Validate Variable
+CALL assert_id_not_null(@macros_id, 'Seed failed: macros_per_pound_and_limits @macros_id could not be resolved');
+
+
 -- ######################################
 -- Inserting Macro Document Version
 -- ######################################
@@ -15,7 +19,7 @@ INSERT INTO macros_per_pound_and_limits_versions
 	user_id,
 	plan_version_id,
 	
-	date_time_of_creation, 
+	date_time_last_edited, 
 	version_number,
 	
 	current_weight_kg, 
@@ -33,3 +37,9 @@ INSERT INTO macros_per_pound_and_limits_versions
 ) VALUES
 
 (@macros_id, @active_user_id, @plan_Version_id, NOW(6), 1, 102.5, 225.5, 25, 1, 2, 30, 0.4, 30, 30, 5000, 400);
+
+-- Set Macros Version ID
+SET @macros_version_id := LAST_INSERT_ID();
+
+-- Validate Variable
+CALL assert_id_not_null(@macros_version_id, 'Seed failed: macros_per_pound_and_limits @macros_version_id could not be resolved');
