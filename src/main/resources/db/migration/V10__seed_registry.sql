1b2d314ac15b51ce1a60035fa5150ee2630f4b42
Tyrone Friday <tyrone.f2000@outlook.com>
2025-12-26 05:58:30 +0000

    Updates Made in this commit: .) Updated Reference_seed data with seed registry
    Next To Do:
    .) Use migration runner & log errors
    .) Edit Screen delete / refresh btn
    
    Previous Updates:
    .)
    
    Errors
    .)

--- src/main/resources/db/migration/V10__seed_registry.sql
+++ src/main/resources/db/migration/V10__seed_registry.sql
@@ -1,6 +1,8 @@
 CREATE TABLE seed_registry
 (
-    seed_key     VARCHAR(64) PRIMARY KEY,
-    entity_type  VARCHAR(32) NOT NULL,
-    entity_id    INT NOT NULL
+    seed_key           VARCHAR(64) PRIMARY KEY,
+    entity_table_name  VARCHAR(32) NOT NULL,
+    entity_id_value  INT NOT NULL,
+
+    UNIQUE unique_id_per_table(entity_table_name, entity_id_value )
 );
--- src/main/resources/db/migration/V11__seed_reference_data.sql
+++ src/main/resources/db/migration/V11__seed_reference_data.sql
@@ -1,31 +1,27 @@
 
 -- #################################################################################################
--- Ingredients Types 
+-- N/A Ingredients Types
 -- #################################################################################################
 
--- Inserting SEED DATA for System Variables
-
-START TRANSACTION;
-
+-- Create Ingredient Type For N/A
 SET @na_ingredient_type_name := 'N/A';
 
-INSERT IGNORE ingredient_types 
+INSERT INTO ingredient_types
 (
 	ingredient_type_name, 
 	is_system
-	
-) VALUES
-
-(@na_ingredient_type_name, TRUE),
-('Un-Assigned', TRUE)
-
-ON DUPLICATE KEY UPDATE -- Incase of duplicate, ensures fields match correctly to new insert is
+)
+VALUES
+    (@na_ingredient_type_name, TRUE),
+    ('Un-Assigned', TRUE)
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert is
     ingredient_type_name = ingredient_type_name;
 
 -- ######################################
--- Variable Setting FOR N/A
+-- Variable Setting FOR N/A Type
 -- ######################################
 
+-- Create Variable for N/A ID
 SELECT ingredient_type_id
 INTO @na_type_id
 FROM ingredient_types
@@ -35,30 +31,39 @@ LIMIT 1;
 -- Validate Variable N/A
 CALL assert_id_not_null(@na_type_id, 'Seed failed: @na_type_id (ingredient types) could not be resolved');
 
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('na_type_id', 'ingredient_types' , @na_type_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value  = new_vals.entity_id_value;
+
 -- #################################################################################################
 -- Ingredient Measurements
 -- #################################################################################################
 
--- Set Variables 
+-- Set Measurement Variables
 SET @litres_measurement_name := 'Litres';
 SET @grams_measurement_name := 'Grams';
 SET @na_measurement_name := 'N/A';
 
--- Inserting SEED DATA for System Variables
+-- ###############################################
+-- Inserting SEED DATA for Measurement Variables
+-- ##############################################
 INSERT INTO measurements 
 ( 
 	is_system,
 	unit_name, 
 	unit_symbol,
-	measured_material_type	
-	
-) VALUES
+	measured_material_type
+)
+VALUES
+    (TRUE, @litres_measurement_name, 'L', 'Liquids'),
+    (TRUE, @grams_measurement_name, 'g', 'Solids'),
+    (TRUE, @na_measurement_name, 'N/A', 'N/A')
 
-(TRUE, @litres_measurement_name, 'L', 'Liquids'),
-(TRUE, @grams_measurement_name, 'g', 'Solids'),
-(TRUE, @na_measurement_name, 'N/A', 'N/A')
-
-ON DUPLICATE KEY UPDATE -- Incase of duplicate, ensures fields match correctly to new insert
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
     unit_name = unit_name;
 
 -- #####################################################
@@ -75,6 +80,14 @@ LIMIT 1;
 -- Litres Variable Validation 
 CALL assert_id_not_null(@litres_id, 'Seed failed: @litres_id (measurement) could not be resolved');
 
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('litres_id', 'measurements' , @litres_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value  = new_vals.entity_id_value;
+
 -- ############################
 -- Grams
 -- ############################
@@ -87,25 +100,44 @@ LIMIT 1;
 -- Grams Variable Validation 
 CALL assert_id_not_null(@grams_id, 'Seed failed: @grams_id (measurement) could not be resolved');
 
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('grams_id', 'measurements' , @grams_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value  = new_vals.entity_id_value;
+
 -- ############################
 -- N/A
 -- ############################
 SELECT measurement_id
-INTO @na_meassurement_id
+INTO @na_measurement_id
 FROM measurements
 WHERE unit_name = @na_measurement_name
 LIMIT 1;
 
 -- N/A Variable Validation 
-CALL assert_id_not_null(@na_meassurement_id, 'Seed failed: @na_meassurement_id (measurement) could not be resolved');
+CALL assert_id_not_null(@na_measurement_id, 'Seed failed: @na_measurement_id (measurement) could not be resolved');
+
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('na_measurement_id', 'measurements' , @na_measurement_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value = new_vals.entity_id_value;
 
 -- #################################################################################################
 -- Ingredient Stores
 -- #################################################################################################
 
+-- Set Stores Variables
 SET @na_store_name := 'No Shop';
 
--- Inserting SEED DATA for System Variables
+-- ###############################################
+-- Create N/A Shop
+-- ###############################################
 INSERT INTO stores 
 (
 	is_system,
@@ -115,12 +147,12 @@ INSERT INTO stores
 (
 	TRUE, @na_store_name
 )
-ON DUPLICATE KEY UPDATE -- Incase of duplicate, ensures fields match correctly to new insert
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
     store_name = store_name;
 
--- ##########################
--- Set Variable
--- ##########################
+-- ###############################################
+-- Set N/A Shop Variable
+-- ###############################################
 SELECT store_id
 INTO @na_store_id
 FROM stores
@@ -130,8 +162,16 @@ LIMIT 1;
 -- Variable Validation 
 CALL assert_id_not_null(@na_store_id, 'Seed failed: Store ID @na_store_id could not be resolved');
 
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('na_store_id', 'stores' , @na_store_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+    entity_id_value = new_vals.entity_id_value;
+
 -- #################################################################################################
--- Inserting SEED DATA for System Variables
+-- Creating N/A Ingredient
 -- #################################################################################################
 
 SET @na_ingredient_name := 'None Of The Above';
@@ -154,17 +194,19 @@ INSERT INTO ingredients_info
 	water_content, 
 	liquid_content, 
 	calories
-	
-) VALUES
+)
+VALUES
 
 (TRUE, @na_measurement_id, @na_ingredient_name, @na_type_id, 0,0,0,0,0,0,0,0,0,0,0,0)
 
-ON DUPLICATE KEY UPDATE -- Incase of duplicate, ensures fields match correctly to new insert
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
 	ingredient_name = ingredient_name;
 
 -- ##########################
--- Set Variable
+-- Set Ingredient Variable
 -- ##########################
+
+-- Create Variable for N/A Ingredient ID
 SELECT ingredient_id
 INTO @na_ingredient_id
 FROM ingredients_info
@@ -174,4 +216,11 @@ LIMIT 1;
 -- Variable Validation 
 CALL assert_id_not_null(@na_ingredient_id, 'Seed failed: Ingredient ID @na_ingredient_id could not be resolved');
 
-COMMIT;
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('na_ingredient_id', 'ingredients_info' , @na_ingredient_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value = new_vals.entity_id_value;
+
