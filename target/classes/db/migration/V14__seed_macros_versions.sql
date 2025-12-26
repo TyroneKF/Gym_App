4dc2af9dc74dca8d852aec1c9d0dc9486fb4cd24
Tyrone Friday <tyrone.f2000@outlook.com>
2025-12-26 09:06:15 +0000

    Updates Made in this commit: .) Script
    Next To Do:
    .) Use migration runner & log errors
    .) Edit Screen delete / refresh btn
    
    Previous Updates:
    .)
    
    Errors
    .)

--- target/classes/db/migration/V14__seed_macros_versions.sql
+++ target/classes/db/migration/V14__seed_macros_versions.sql
@@ -1,6 +1,28 @@
--- ######################################
+-- ###############################################################################
+-- Set Variables
+-- ###############################################################################
+-- Get Active User From Seed
+SELECT entity_id_value
+INTO @active_user_id
+FROM seed_registry
+WHERE seed_key = 'active_user_id';
+
+-- Variable Validation
+CALL assert_id_not_null(@active_user_id, 'Seed failed: no active user could be resolved');
+
+-- Get Active Plan Version From Seed
+SELECT entity_id_value
+INTO @plan_Version_id
+FROM seed_registry
+WHERE seed_key = 'plan_Version_id';
+
+-- Variable Validation
+CALL assert_id_not_null(@plan_Version_id, 'Seed failed: @plan_Version_id could be resolved');
+
+-- ###############################################################################
+-- Macro
+-- ###############################################################################
 -- Inserting Macro
--- ######################################
 INSERT INTO macros_per_pound_and_limits (macros_ID) VALUES
 (null);
 
@@ -34,8 +56,8 @@ INSERT INTO macros_per_pound_and_limits_versions
 	
 	additional_calories
 	
-) VALUES
-
+)
+VALUES
 (@macros_id, @active_user_id, @plan_Version_id, NOW(6), 1, 102.5, 225.5, 25, 1, 2, 30, 0.4, 30, 30, 5000, 400);
 
 -- Set Macros Version ID
@@ -44,4 +66,11 @@ SET @macros_version_id := LAST_INSERT_ID();
 -- Validate Variable
 CALL assert_id_not_null(@macros_version_id, 'Seed failed: macros_per_pound_and_limits @macros_version_id could not be resolved');
 
+-- Insert Into Seed Registry Table
+INSERT INTO seed_registry (seed_key, entity_table_name, entity_id_value)
+VALUES
+    ('macros_version_id', 'macros_per_pound_and_limits_versions' , @macros_version_id)
+AS new_vals
+ON DUPLICATE KEY UPDATE -- In case of duplicate, ensures fields match correctly to new insert
+	entity_id_value = new_vals.entity_id_value;
 
