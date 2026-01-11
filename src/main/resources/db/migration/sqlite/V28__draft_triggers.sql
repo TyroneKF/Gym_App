9a9b2c5564fc6875f4ac8051243019bf0c1485a2
Tyrone Friday <tyrone.f2000@outlook.com>
2026-01-11 05:12:13 +0000

    Updates Made in this commit: .) Sqlite Triggers Split & Updated
    Next To Do:
    
    .) Views for drafts / does the logic need to be duplicated
    
    .) Meal_Plan_Screen.java initialization scripts
    .) Use migration runner & log errors
    
    .) Edit Screen delete / refresh btn
    
    Previous Updates:
    .)
    
    Errors
    .)

--- src/main/resources/db/migration/sqlite/V28__draft_triggers.sql
+++ src/main/resources/db/migration/sqlite/V28__draft_triggers.sql
@@ -2,17 +2,17 @@
 -- draft_plans | Prevent Certain Rows from being updated
 -- ###########################################################################
     CREATE TRIGGER trg_draft_plans_allow_only_safe_updates
-    BEFORE UPDATE ON draft_plans
+    BEFORE UPDATE OF user_id ON draft_plans
     FOR EACH ROW
-    WHEN
-        -- Immutable Columns (Not Allowed)
-        NEW.user_id  IS NOT OLD.user_id
+        BEGIN
+            SELECT RAISE(ABORT, 'UPDATE Trigger - draft_plans only mutable columns can change!');
+        END;
 
-        -- Mutable Columns (Implicitly Allowed):
-            -- date_time_last_edited
-    BEGIN
-            SELECT RAISE(ABORT, "Trigger - 'draft_plans' only mutable columns can change!");
-    END;
+    -- Immutable Columns (Not Allowed)
+        -- user_id
+
+    -- Mutable Columns (Implicitly Allowed):
+        -- date_time_last_edited
 
 -- ###########################################################################
 -- draft_macros_per_pound_and_limits |
@@ -24,29 +24,28 @@
     -- ##########################################
     -- Allows only certain rows to be updated
     -- ##########################################
-    CREATE TRIGGER trg_draft_macros_per_pound_and_limits_allow_only_safe_updates
-    BEFORE UPDATE ON draft_macros_per_pound_and_limits
-    FOR EACH ROW
-    WHEN
-        -- Immutable Columns (Not Allowed)
-        NEW.user_id IS NOT OLD.user_id
+        CREATE TRIGGER trg_draft_macros_per_pound_and_limits_allow_only_safe_updates
+        BEFORE UPDATE OF user_id ON draft_macros_per_pound_and_limits
+        FOR EACH ROW
+            BEGIN
+                SELECT RAISE(ABORT, 'UPDATE Trigger - draft_macros_per_pound_and_limits only mutable columns can change! ');
+            END;
 
-        -- Mutable Columns (Implicitly Allowed):
-            --  current_weight_kg
-            --  current_weight_in_pounds
-            --  body_fat_percentage
-            --  protein_per_pound
-            --  carbohydrates_per_pound
-            --  fibre
-            --  fats_per_pound
-            --  saturated_fat_limit
-            --  salt_limit
-            --  water_target
-            --  additional_calories
+            -- Immutable Columns (Not Allowed)
+                -- user_id
 
-    BEGIN
-        SELECT RAISE(ABORT, ' Trigger - draft_macros_per_pound_and_limits only mutable columns can change! ');
-    END;
+            -- Mutable Columns (Implicitly Allowed):
+                --  current_weight_kg
+                --  current_weight_in_pounds
+                --  body_fat_percentage
+                --  protein_per_pound
+                --  carbohydrates_per_pound
+                --  fibre
+                --  fats_per_pound
+                --  saturated_fat_limit
+                --  salt_limit
+                --  water_target
+                --  additional_calories
 
 -- ###########################################################################
 -- draft_meals_in_plan Trigger | Prevent Certain Rows From Being Updated
@@ -59,54 +58,51 @@
     -- Prevent Certain Rows From Being Updated
     -- ##########################################
         CREATE TRIGGER trg_draft_meals_in_plan_allow_only_safe_updates
-        BEFORE UPDATE ON draft_meals_in_plan
+        BEFORE UPDATE OF meal_in_plan_id, plan_id
+        ON draft_meals_in_plan
         FOR EACH ROW
-        WHEN
-                -- Immutable Columns (Not Allowed)
-                NEW.meal_in_plan_id IS NOT OLD.meal_in_plan_id
-                OR NEW.plan_id IS NOT OLD.plan_id
-    
-                -- Mutable Columns (Implicitly Allowed):
-                    -- meal_name
-                    -- meal_time
-                    -- date_time_last_edited
-    
-        BEGIN
-            SELECT RAISE(ABORT, ' Trigger -  draft_meals_in_plan table only mutable columns can change!');
-        END;
-    
+            BEGIN
+                SELECT RAISE(ABORT, 'UPDATE Trigger -  draft_meals_in_plan table only mutable columns can change!');
+            END;
+
+         -- Immutable Columns (Not Allowed)
+            -- meal_in_plan_id
+            -- plan_id
+
+        -- Mutable Columns (Implicitly Allowed):
+            -- meal_name
+            -- meal_time
+            -- date_time_last_edited
+
+    -- ##########################################
+     -- On Update : Update Meal External Update
+     -- ##########################################
+         CREATE TRIGGER trg_on_draft_meals_update_then_update_plans_date
+         AFTER UPDATE ON draft_meals_in_plan
+         FOR EACH ROW
+             BEGIN
+
+                 -- Update plans Last Edit time when meals are updated
+                 UPDATE draft_plans
+                     SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                 WHERE  plan_id = OLD.plan_id;
+
+             END;
+
     -- ##########################################
     -- On INSERT : Update Meal
     -- ##########################################
         CREATE TRIGGER trg_on_draft_meals_insert_then_update_plans_date
         AFTER INSERT ON draft_meals_in_plan
         FOR EACH ROW
-        BEGIN
+            BEGIN
 
-            -- Update plans Last Edit time when meals are updated
-            UPDATE draft_plans 
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  plan_id = NEW. plan_id;
+                -- Update plans Last Edit time when meals are updated
+                UPDATE draft_plans
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE  plan_id = NEW.plan_id;
 
-        END;
-
-    -- ##########################################
-    -- On Update : Update Meal External Update
-    -- ##########################################
-        CREATE TRIGGER trg_on_draft_meals_update_then_update_plans_date
-        AFTER UPDATE ON draft_meals_in_plan
-        FOR EACH ROW
-        WHEN
-             NEW.meal_name    IS NOT OLD.meal_name
-             OR NEW.meal_time IS NOT OLD.meal_time
-        BEGIN
-
-            -- Update plans Last Edit time when meals are updated
-            UPDATE draft_plans 
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  plan_id = OLD.plan_id;
-
-        END;
+            END;
 
     -- ##########################################
     -- On Delete : Update Sub-Meal
@@ -114,14 +110,14 @@
         CREATE TRIGGER trg_on_draft_meals_delete_then_update_plans_date
         AFTER DELETE ON draft_meals_in_plan
         FOR EACH ROW
-        BEGIN
+            BEGIN
 
-            -- Update plans Last Edit time when meals are updated
-            UPDATE draft_plans 
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  plan_id = OLD.plan_id;
+                -- Update plans Last Edit time when meals are updated
+                UPDATE draft_plans
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE  plan_id = OLD.plan_id;
 
-        END;
+            END;
 
 -- ###########################################################################
 -- draft_divided_meal_sections Trigger
@@ -131,24 +127,40 @@
     */
 
     -- ##########################################
-    -- Allows Only Certain Rows To Be Updated
+    -- On Update: Prevents Certain Rows Updating
     -- ##########################################
         CREATE TRIGGER trg_draft_divided_meal_sections_allow_only_safe_updates
-        BEFORE UPDATE ON draft_divided_meal_sections
+        BEFORE UPDATE OF div_meal_sections_id, draft_meal_in_plan_id, plan_id
+        ON draft_divided_meal_sections
         FOR EACH ROW
-        WHEN
-            -- Immutable Columns (Not Allowed)
-            NEW.div_meal_sections_id IS NOT OLD.div_meal_sections_id
-            AND NEW.draft_meal_in_plan_id IS NOT OLD.draft_meal_in_plan_id
-            AND NEW.plan_id IS NOT OLD.plan_id
+            BEGIN
+                SELECT RAISE(ABORT,'UPDATE Trigger - draft_divided_meal_sections only mutable columns can change!');
+            END;
 
-            -- Mutable Columns (Implicitly Allowed):
-                -- sub_meal_name
-                -- sub_meal_time
-                -- date_time_last_edited
-        BEGIN
-            SELECT RAISE(ABORT,'Trigger - draft_divided_meal_sections only mutable columns can change!');
-        END;
+         -- Immutable Columns (Not Allowed)
+            -- div_meal_sections_id
+            -- draft_meal_in_plan_id
+            -- plan_id
+
+        -- Mutable Columns (Implicitly Allowed):
+            -- sub_meal_name
+            -- sub_meal_time
+            -- date_time_last_edited
+
+    -- ##########################################
+    -- After Update : External Table Update
+    -- ##########################################
+        CREATE TRIGGER trg_on_draft_sub_meals_update_then_update_meals_date
+        AFTER UPDATE ON draft_divided_meal_sections
+        FOR EACH ROW
+            BEGIN
+
+                -- Update Sub-meal Last Edit time when ingredients are updated
+                UPDATE draft_meals_in_plan
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE  draft_meal_in_plan_id = OLD.draft_meal_in_plan_id;
+
+            END;
 
     -- ##########################################
     -- On INSERT : Update Sub-Meal
@@ -156,31 +168,14 @@
         CREATE TRIGGER trg_on_draft_sub_meals_insert_then_update_meals_date
         AFTER INSERT ON draft_divided_meal_sections
         FOR EACH ROW
-        BEGIN
+            BEGIN
 
-            -- Update Sub-meal Last Edit time when ingredients are updated
-            UPDATE draft_meals_in_plan
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  draft_meal_in_plan_id = NEW. draft_meal_in_plan_id;
+                -- Update Sub-meal Last Edit time when ingredients are updated
+                UPDATE draft_meals_in_plan
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE  draft_meal_in_plan_id = NEW.draft_meal_in_plan_id;
 
-        END;
-
-    -- ##########################################
-    -- On Update : Update Meal External Update
-    -- ##########################################
-        CREATE TRIGGER trg_on_draft_sub_meals_update_then_update_meals_date
-        AFTER UPDATE ON draft_divided_meal_sections
-        FOR EACH ROW
-        WHEN
-             NEW.date_time_last_edited  IS NOT OLD.date_time_last_edited
-        BEGIN
-
-            -- Update Sub-meal Last Edit time when ingredients are updated
-            UPDATE draft_meals_in_plan
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  draft_meal_in_plan_id = OLD.draft_meal_in_plan_id;
-
-        END;
+            END;
 
     -- ##########################################
     -- On Delete : Update Sub-Meal
@@ -188,14 +183,14 @@
         CREATE TRIGGER trg_on_draft_sub_meals_delete_then_update_meals_date
         AFTER DELETE ON draft_divided_meal_sections
         FOR EACH ROW
-        BEGIN
+            BEGIN
 
-            -- Update Sub-meal Last Edit time when ingredients are updated
-            UPDATE draft_meals_in_plan
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE  draft_meal_in_plan_id = OLD.draft_meal_in_plan_id;
+                -- Update Sub-meal Last Edit time when ingredients are updated
+                UPDATE draft_meals_in_plan
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE  draft_meal_in_plan_id = OLD.draft_meal_in_plan_id;
 
-        END;
+            END;
 
 -- ###########################################################################
 -- draft_ingredients_in_sections_of_meal Trigger
@@ -208,19 +203,36 @@
     -- Allows Only Certain Rows To Be Updated
     -- ##########################################
         CREATE TRIGGER trg_ingredients_in_meal_versions_allow_only_safe_updates
-        BEFORE UPDATE ON draft_ingredients_in_sections_of_meal
+        BEFORE UPDATE OF draft_div_meal_sections_id
+        ON draft_ingredients_in_sections_of_meal
         FOR EACH ROW
-        WHEN
-            -- Immutable Columns (Not Allowed)
-            NEW.draft_div_meal_sections_id IS NOT OLD.draft_div_meal_sections_id
+            BEGIN
+                SELECT RAISE(ABORT,'UPDATE Trigger - draft_ingredients_in_sections_of_meal only mutable columns can change!');
+            END;
 
-            -- Mutable Columns (Implicitly Allowed):
-                -- ingredient_id
-                -- quantity
-                -- pdid
-        BEGIN
-            SELECT RAISE(ABORT,'Trigger - draft_ingredients_in_sections_of_meal only mutable columns can change!');
-        END;
+        -- Immutable Columns (Not Allowed)
+            -- draft_div_meal_sections_id
+
+        -- Mutable Columns (Implicitly Allowed):
+            -- ingredient_id
+            -- quantity
+            -- pdid
+
+    -- ##########################################
+    -- On Update : Update Sub-Meal Date
+    -- ##########################################
+        CREATE TRIGGER trg_on_draft_ingredients_update_then_update_sub_meals_date
+        AFTER UPDATE
+        ON draft_ingredients_in_sections_of_meal
+        FOR EACH ROW
+            BEGIN
+
+                -- Update Sub-meal Last Edit time when ingredients are updated
+                UPDATE draft_divided_meal_sections
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE draft_div_meal_sections_id = OLD.draft_div_meal_sections_id;
+
+            END;
 
     -- ##########################################
     -- On INSERT : Update Sub-Meal Date
@@ -228,33 +240,14 @@
         CREATE TRIGGER trg_on_draft_ingredients_insert_then_update_sub_meals_date
         AFTER INSERT ON draft_ingredients_in_sections_of_meal
         FOR EACH ROW
-        BEGIN
+            BEGIN
 
-            -- Update Sub-meal Last Edit time when ingredients are updated
-            UPDATE draft_divided_meal_sections
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE draft_div_meal_sections_id = NEW.draft_div_meal_sections_id;
+                -- Update Sub-meal Last Edit time when ingredients are updated
+                UPDATE draft_divided_meal_sections
+                    SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
+                WHERE draft_div_meal_sections_id = NEW.draft_div_meal_sections_id;
 
-        END;
-
-    -- ##########################################
-    -- On Update : Update Sub-Meal Date
-    -- ##########################################
-        CREATE TRIGGER trg_on_draft_ingredients_update_then_update_sub_meals_date
-        AFTER UPDATE ON draft_ingredients_in_sections_of_meal
-        FOR EACH ROW
-        WHEN
-             NEW.quantity IS NOT OLD.quantity
-             OR NEW.ingredient_id IS NOT OLD.ingredient_id
-             OR NEW.pdid IS NOT OLD.pdid
-        BEGIN
-
-            -- Update Sub-meal Last Edit time when ingredients are updated
-            UPDATE draft_divided_meal_sections
-                SET date_time_last_edited = strftime('%Y-%m-%d %H:%M:%f', 'now')
-            WHERE draft_div_meal_sections_id = OLD.draft_div_meal_sections_id;
-
-        END;
+            END;
 
     -- ##########################################
     -- On Delete : Update Sub-Meal Date
