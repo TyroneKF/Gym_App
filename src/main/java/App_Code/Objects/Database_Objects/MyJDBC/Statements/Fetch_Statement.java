55222a5fb0db3a6ce03df215a5254eb6d0593008
Tyrone Friday <tyrone.f2000@outlook.com>
2026-02-02 21:08:45 +0000

    Updates Made in this commit: .) Created MYSQL objects & removed unused classes
    Next To Do:
    .)
    
    Previous Updates:
    .)
    
    Errors
    .)

---
+++ src/main/java/App_Code/Objects/Database_Objects/MyJDBC/Statements/Fetch_Statement.java
@@ -0,0 +1,12 @@
+package App_Code.Objects.Database_Objects.MyJDBC.Statements;
+
+public class Fetch_Statement extends Parent_Statement
+{
+    //##################################################################################################################
+    // Constructor
+    //##################################################################################################################
+    public Fetch_Statement(String query, Object[] params)
+    {
+        super(query, params);
+    }
+}
